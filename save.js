df6bf535a9b5fbe9360c5122e5890b1bdebbf517
DevJewellers <66473483+DevJewellers@users.noreply.github.com>
2020-09-29 16:53:31 +0530

    Update save.js

--- save.js
+++ save.js
@@ -274,7 +274,7 @@ function get_data(mydata){
 
 
 var qr=document.getElementById("qrImg")
-	qr.src="https://chart.googleapis.com/chart?chs=500x500&cht=qr&choe=UTF-8&chld=l|0&chl=https://devjewellers.github.io/invoice/?dev-jewellers-"+myData.invoice_no;
+	qr.src="https://chart.googleapis.com/chart?chs=500x500&cht=qr&choe=UTF-8&chld=l|0&chl=https://devjewellers.github.io/invoice/?"+btoa("dev-jewellers-"+document.getElementById("inno").value);
 
 
 
